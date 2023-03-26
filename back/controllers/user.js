cce6c043cfdf3e3aff4b383c466b18294c2d8548
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-03-26 21:45:19 +0200

    add token

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -16,27 +16,32 @@ exports.signup = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
+// Fonction Login : RANDOW_TOKEN_SECRET à remplacer par chaîne aléatoire + longue pour production
+
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
       .then(user => {
           if (!user) {
-              return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+              return res.status(401).json({ error: 'Utilisateur non trouvé !' });
           }
           bcrypt.compare(req.body.password, user.password)
               .then(valid => {
                   if (!valid) {
-                      return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                      return res.status(401).json({ error: 'Mot de passe incorrect !' });
                   }
                   res.status(200).json({
                       userId: user._id,
-                      token: 'TOKEN'
+                      token: jwt.sign(
+                          { userId: user._id },
+                          'RANDOM_TOKEN_SECRET',
+                          { expiresIn: '24h' }
+                      )
                   });
               })
               .catch(error => res.status(500).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
 };
-
 // exports.deleteUser = (req, res, next) => {
 //   User.deleteOne({ _id: req.params.id })
 //     .then(() => res.status(200).json({ message: 'Utilisateur supprimé !' }))
