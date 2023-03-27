1f0aee8642eb316f7067c1f5ae11572daaaeb0cb
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-03-27 03:03:08 +0200

    validateInput to rework & verify winston

--- back/app.js
+++ back/app.js
@@ -53,18 +53,3 @@ app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
-
-
-
-
-
-// const rateLimit = require("express-rate-limit");
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100 // limit each IP to 100 requests per windowMs
-// });
-
-
-
-// // Limit the number of requests for each IP
-// app.use(limiter);
--- back/routes/user.js
+++ back/routes/user.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
-const { validateUserInput } = require('../middleware/validation');
+const { validateUserInput } = require('../middleware/validate-user-input');
 
-router.post('/signup', validateUserInput, userCtrl.signup);
+router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
