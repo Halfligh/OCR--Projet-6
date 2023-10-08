fbbb457406ef6b3311d65b5caa0ff8230009c2f0
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-10-08 20:50:57 +0200

    last clean

--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -14,7 +14,7 @@ const limiter = rateLimit({
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.post("/", auth, limiter, multer, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.put("/:id", auth, limiter, multer, sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
 router.post("/:id/like", auth, sauceCtrl.likeSauce);
 
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
 const userCtrl = require("../controllers/user");
-const { validateUserInput } = require("../middlewares/validate-user-input");
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
